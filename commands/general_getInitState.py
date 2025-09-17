b8e4fb9ee32ed7af82d3c1f7d4089c54868bde93
Mima2370 <michielvde@hotmail.be>
2025-09-17 11:52:34 +0200

    Fix duplicating items

--- commands/general_getInitState.py
+++ commands/general_getInitState.py
@@ -1,6 +1,7 @@
 import time
 from commands.buddy_getAll import run_buddy_checks
-from deepmerge import always_merger
+from deepmerge import Merger
+from copy import deepcopy
 
 def handle_getInitState(request, user_id, rpcResult, items_to_add_to_obj, json_data, init_data):
     rpcResult["i"] = request["i"]
@@ -28,6 +29,15 @@ def handle_getInitState(request, user_id, rpcResult, items_to_add_to_obj, json_d
     # Run buddy.getAll
     run_buddy_checks(request["t"], json_data)
 
-    rpcResult["r"] = always_merger.merge(json_data, init_data) # Deepmerge both global init and personal user init.
+    merger = Merger(
+        [(dict, ["merge"]), (list, ["override"]), (set, ["override"])],
+        ["override"],
+        ["override"]
+    ) # Merge dicts, but override lists and sets (to prevent duplicating things)
+
+    rpcResult["r"] = deepcopy(merger.merge(json_data, init_data)) # Deepmerge both global init and personal user init.
+
+    # Don't send password and token to the game
     # To-do when switching to an acutal database: don't store password between the game data!!!!!!!
-    
+    del rpcResult["r"]["playerData"]["password"]
+    del rpcResult["r"]["playerData"]["token"]
