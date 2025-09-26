ab6740c1b55a1af2ca4041c6c80d268ff488e51e
Mima2370 <37240450+Mima2370@users.noreply.github.com>
2025-09-26 13:50:30 +0200

    Don't clone init data into user data

--- commands/general_getInitState.py
+++ commands/general_getInitState.py
@@ -35,7 +35,8 @@ def handle_getInitState(request, user_id, rpcResult, items_to_add_to_obj, json_d
         ["override"]
     ) # Merge dicts, but override lists and sets (to prevent duplicating things)
 
-    rpcResult["r"] = deepcopy(merger.merge(json_data, init_data)) # Deepmerge both global init and personal user init.
+    rpcResult["r"] = deepcopy(json_data)
+    merger.merge(rpcResult["r"], init_data) # Deepmerge both global init and personal user init.
 
     # Don't send password and token to the game
     # To-do when switching to an acutal database: don't store password between the game data!!!!!!!
