7197bfeaee05dcb620eecde35f73b1124d59db3e
Mima2370 <37240450+Mima2370@users.noreply.github.com>
2025-10-19 19:49:40 +0200

    Remove useless import

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ from commands import *
 from werkzeug import Request
 from bundle import TEMPLATES_DIR, STUB_DIR, STYLES_DIR, ASSETS_DIR
 from src.utils import get_level_from_xp
-from src.utils import get_crafting_level_from_xp
 import src.userManager as userManager
 import src.configHandler as configHandler
 
