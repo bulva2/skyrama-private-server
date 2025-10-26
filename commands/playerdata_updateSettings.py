87023967cef6eb0081a1d3159c2be1358c183206
Mima2370 <37240450+Mima2370@users.noreply.github.com>
2025-10-26 13:03:05 +0100

    Add show/hide buddy flag setting

--- commands/playerdata_updateSettings.py
+++ commands/playerdata_updateSettings.py
@@ -10,3 +10,5 @@ def handle_updateSettings(request, user_id, rpcResult, items_to_add_to_obj, json
     json_data["playerData"]["sound_on"] = request["p"]["sound_on"]
     json_data["playerData"]["superfuel_on"] = request["p"]["superfuel_on"]
     json_data["playerData"]["animations_on"] = request["p"]["animations_on"]
+    if "buddyflag_on" in request["p"]: # For compatibility with original Skyrama
+        json_data["playerData"]["buddyflag_on"] = request["p"]["buddyflag_on"]
