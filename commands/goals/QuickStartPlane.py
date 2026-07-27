50059c53ad192a64f8794423359d0aca75e33760
bulva2 <tomik7700@email.cz>
2026-07-27 23:42:46 +0200

    feat: update to postgres 18, properly handle quick start quest

--- commands/goals/QuickStartPlane.py
+++ commands/goals/QuickStartPlane.py
@@ -1,6 +1,15 @@
+from src.enums import PlaneState
+
+_QUICK_START_STATES = (PlaneState.QUICK_START.value, PlaneState.QUICK_BUDDY_SERVE.value)
+
 def handle_QuickStartPlane(request, user_id, json_data, task, task_number, init_data, quest_seq):
-    if request["m"] == "planes.takeMeans":
-        if request["p"]["plane_id"] == 0: # There's only one mission of this type, do it the lazy way.
-            json_data["goals"]["goals"][quest_seq]["tasks"][task_number]["num_completed"] = json_data["goals"]["goals"][quest_seq]["tasks"][task_number]["num_completed"] + 1
-    
+    if request["m"] == "planes.setState":
+        try:
+            flight_status = int(request["p"]["flight_status"])
+        except (KeyError, TypeError, ValueError):
+            return json_data
+
+        if flight_status in _QUICK_START_STATES:
+            json_data["goals"]["goals"][quest_seq]["tasks"][task_number]["num_completed"] += 1
+
     return json_data
